57d4190cf72c171a7017b50d7c433ad732269d0d
BRP7 <119129720+BRP7@users.noreply.github.com>
2024-06-03 13:11:33 +0530

    JS

--- configuration.js
+++ configuration.js
@@ -3,18 +3,20 @@ var Configuration = Class.create();
 
 Configuration.prototype = {
     initialize: function (options) {
-        this.containerId = options.containerId;
+        // this.containerId = options.containerId;
         this.redirectedUrl = options.redirected_url;
+        this.loadUrl = options.loadUrl;
         this.tableCounter = 0;
         this.rowCounter = 1;
         this.isFirstRowAddedByScript = false;
-        this.loadUploadContainer();
         this.formKey = options.form_key;
+        this.setupEventHandlers();
+        this.loadConfigurationData();
     },
 
-    loadUploadContainer: function () {
+    setupEventHandlers: function () {
         var self = this;
-        j("#add_new_row_btn").on("click", function (event) {
+        j("#add_new_table_btn").on("click", function (event) {
             event.preventDefault();
             self.addNewTable();
         });
@@ -25,30 +27,105 @@ Configuration.prototype = {
         });
     },
 
+    loadConfigurationData: function () {
+        var self = this;
+        var url = window.location.href;
+        var urlParts = url.split('/');
+        var idIndex = urlParts.indexOf('id');
+        if (idIndex !== -1 && idIndex < urlParts.length - 1) {
+            var configId = urlParts[idIndex + 1];
+            j.ajax({
+                url: self.loadUrl,
+                data: { config_id: configId },
+                success: function (response) {
+                    var tables = JSON.parse(response);
+                    console.log(tables);
+                    if (tables.status !== 'error') {
+                        tables.forEach(function (table) {
+                            self.isFirstRowAddedByScript = true;
+                            self.addNewTable();
+                            table.forEach(function (row) {
+                                var lastTableIndex = self.tableCounter - 1;
+                                var table = j("#table_" + lastTableIndex);
+                                var tr = j("<tr></tr>");
+                                tr.append(j("<td></td>").append(self.createDropDown(["subject", "from", "to"], "condition_name_" + row.group_id).val(row.condition_name)));
+                                tr.append(j("<td></td>").append(self.createDropDown(["=", "%Like%", "Like", ">=", "<=", "!="], "operator_" + row.group_id).val(row.operator)));
+                                tr.append(j("<td></td>").append(j("<input>").attr({ type: "text", name: "value_" + row.group_id }).val(row.value)));
+                                tr.append(j("<td></td>").append(j("<button></button>").text("Add").addClass("add-button").on("click", function (event) {
+                                    event.preventDefault();
+                                    self.handleAdd(this, lastTableIndex);
+                                }).add(j("<button></button>").text("Delete").addClass("remove-button").on("click", function (event) {
+                                    event.preventDefault();
+                                    self.handleDelete(this);
+                                }))));
+                                tr.append(j("<td></td>").attr("id", "dispatch_event_" + lastTableIndex).attr("rowspan", 1).append(j("<input>").attr({ type: "text", id: "dispatchevent", name: "event_" + row.group_id }).val(row.event_name)));
+                                table.append(tr);
+                                self.updateRowspan(lastTableIndex);
+                            });
+                        });
+                    }
+                }
+            });
+        }
+    },
+
     addNewTable: function () {
         var self = this;
         self.rowCounter = 1;
         var tableContainer = j("#main_container");
         var table = j("<table></table>").attr("id", "table_" + self.tableCounter).appendTo(tableContainer);
-        var tableHeader = ["Condition", "Condition Operator", "Condition Value", "Actions", "Dispatch Event"];
+        var tableHeader = ["Condition", "Condition Operator", "Condition Value", "Actions", "Dispatch Event", "Delete Table"];
         var tr = j("<tr></tr>");
         tableHeader.forEach(function (header) {
             tr.append(j("<th></th>").text(header));
         });
         table.append(tr);
-
+    
         if (!self.isFirstRowAddedByScript) {
-            self.addRow(self.tableCounter); // Add row only if the first row is not added by the script
+            // Add row only if the first row is not added by the script
+            self.addRow(self.tableCounter);
         }
+    
+        var deleteButton = j("<button></button>").text("Delete Table").addClass("delete-table-button").data("table-id", self.tableCounter).on("click", function() {
+            var tableId = j(this).data("table-id");
+            self.handleDeleteTable(tableId);
+        });
+        tr.append(deleteButton);
+    
         self.tableCounter++;
     },
+    
+    handleDeleteTable: function (tableId) {
+        var self = this;
+        j("#table_" + tableId).remove();
+        self.updateTableCounter();
+    },
+    
+    
+    updateTableCounter: function () {
+        var self = this;
+        self.tableCounter--;
+        // Update IDs of remaining tables and dispatch event fields
+        j("#main_container table").each(function (index) {
+            var newTableId = "table_" + index;
+            j(this).attr("id", newTableId);
+            j(this).find("button.delete-table-button").attr("data-table-id", index);
+            j(this).find("input[name^='event_']").attr("name", "event_" + index);
+            j(this).find("td[id^='dispatch_event_']").attr("id", "dispatch_event_" + index);
+            self.updateRowspan(index);
+        });
+        // // Update group ids in data array
+        // self.updateDataArray();
+    },
+    
+    
 
     addRow: function (tableId) {
         var self = this;
         var table = j("#table_" + tableId);
         var tr = j("<tr></tr>").attr("id", "row_" + self.rowCounter);
 
-        tr.append(j("<td></td>").append(self.createDropDown(["subject", "from", "to"], "condition_" + self.rowCounter)));
+        tr.append(j("<td></td>").append(self.createDropDown(["subject", "from", "to"], "condition_name_" + self.rowCounter)));
         tr.append(j("<td></td>").append(self.createDropDown(["=", "%Like%", "Like", ">=", "<=", "!="], "operator_" + self.rowCounter)));
         tr.append(j("<td></td>").append(j("<input>").attr({ type: "text", name: "value_" + self.rowCounter })));
 
@@ -154,9 +231,10 @@ Configuration.prototype = {
         j("#main_container table").each(function (tableIndex) {
             var table = [];
             j(this).find("tr:gt(0)").each(function () {
+                console.log(j(this).find("select[name^='condition_name_']").val());
                 var row = {
                     group_id: tableIndex,
-                    condition_name: j(this).find("select[name^='condition_']").val(),
+                    condition_name: j(this).find("select[name^='condition_name_']").val(),
                     operator: j(this).find("select[name^='operator_']").val(),
                     value: j(this).find("input[name^='value_']").val(),
                 };
@@ -169,19 +247,12 @@ Configuration.prototype = {
         var url = window.location.href;
         var urlParts = url.split('/');
         var idIndex = urlParts.indexOf('id');
-        if (idIndex !== -1 && idIndex < urlParts.length - 1) {
-            var configId = urlParts[idIndex + 1];
-            console.log(configId);
-        } else {
-            console.log('id parameter not found in the URL');
-        }
-
-        var form_key = this.formKey;
-        console.log(form_key);
+        var configId = idIndex !== -1 && idIndex < urlParts.length - 1 ? urlParts[idIndex + 1] : null;
+        console.log(JSON.stringify(tables));
         j.ajax({
             type: "POST",
-            url: this.redirectedUrl,
-            data: { tables: JSON.stringify(tables), form_key: form_key, config_id: configId },
+            url: self.redirectedUrl,
+            data: { tables: JSON.stringify(tables), form_key: self.formKey, config_id: configId },
             success: function (response) {
                 console.log(response);
                 console.log("Configuration saved successfully!");
